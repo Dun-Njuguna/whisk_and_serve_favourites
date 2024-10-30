ebc0ed98e32e59555a9a22f94d0905e5b4e40e27
Duncan Kariuki <duncankariuki60@gmail.com>
2024-10-30 08:19:50 +0300

    fix

--- lib/presentation/favourites.dart
+++ lib/presentation/favourites.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:whisk_and_serve/core/widgets/base_scaffold.dart';
 
 class Favourites extends StatelessWidget {
   const Favourites({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const BaseScaffold(
-      child: Center(
+    return const Scaffold(
+      body: Center(
         child: Text("favourites"),
       ),
     );
